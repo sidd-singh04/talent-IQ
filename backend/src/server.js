459cbd71b700889de0f36a4e8ad2c53e5a5427a4
sidd-singh04 <siddharthsingh04.singh@gmail.com>
2026-02-10 18:32:20 +0530

    Move nodemon to devDependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,12 @@ import { ENV } from "./lib/env.js";
 
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.status(200).json({message:"Api Running"});
-})
-app.listen(ENV.PORT, ()=>{
-    console.log(`Server is running on port ${ENV.PORT}`);
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API Running" });
+});
+
+const PORT = ENV.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
