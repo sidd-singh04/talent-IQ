99a054f3f0a276186575bc8621d44237f6a3a2af
sidd-singh04 <siddharthsingh04.singh@gmail.com>
2026-02-10 18:11:36 +0530

    First Commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.status(200).json({message:"Api Running"});
+})
+app.listen(ENV.PORT, ()=>{
+    console.log(`Server is running on port ${ENV.PORT}`);
+})
