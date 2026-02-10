6eb131dec01a1eccf0ca954e7ebd323557cb04b2
sidd-singh04 <siddharthsingh04.singh@gmail.com>
2026-02-10 22:35:29 +0530

    db and tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+import { ENV } from "./env.js"
+
+export const connectDB = async() =>{
+    try {
+        const conn = await mongoose.connect(ENV.DB_URL);
+        console.log("Database Connected Successfully✅");
+    } catch (error) {
+        console.error(error);
+    }
+}
