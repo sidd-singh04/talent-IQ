9f171294e4358c14ea89cf50cae4c514ec4fb5b0
sidd-singh04 <siddharthsingh04.singh@gmail.com>
2026-02-14 10:12:09 +0530

    Landing Page Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProblemsPage from "./pages/ProblemsPage";
+import DashboardPage from "./pages/DashboardPage";
 
 const App = () => {
   const { isSignedIn, isLoaded } = useUser();
 
-  if (!isLoaded) return null;
+  if (!isLoaded) return null;  // To get rid of the flickering effect
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={!isSignedIn ? <HomePage />: <Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage/>: <Navigate to="/" />} />
         <Route path="/about" element={<AboutPage />} />
         <Route
           path="/problems"
@@ -22,6 +24,8 @@ const App = () => {
       </Routes>
 
       {/* Toast container */}
+
+
       <Toaster position="top-right" />
     </>
   );
