fdc45b81b971b1cff11bfbeb4519ada157208eeb
sidd-singh04 <siddharthsingh04.singh@gmail.com>
2026-02-15 13:30:17 +0530

    axios fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,27 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+//   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
+// });
+
+// export default axiosInstance;
+
 import axios from "axios";
+import { clerk } from "@clerk/clerk-react";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
+});
+
+axiosInstance.interceptors.request.use(async (config) => {
+  const token = await clerk.session?.getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default axiosInstance;
