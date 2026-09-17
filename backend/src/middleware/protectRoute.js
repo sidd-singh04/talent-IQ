2285a5df5860d58aa22fa72b734f4ca2a4dbc9cb
sidd-singh04 <siddharthsingh04.singh@gmail.com>
2026-09-17 11:56:19 +0530

    fix session authentication flow

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,3 +1,34 @@
+// import { requireAuth } from "@clerk/express";
+// import User from "../models/User.js";
+
+// export const protectRoute = [
+//   requireAuth(),
+//   async (req, res, next) => {
+//     try {
+//       const clerkId = req.auth.userId;
+
+
+//       if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
+
+//       // find user in db by clerk ID
+//       const user = await User.findOne({ clerkId });
+
+//       if (!user) return res.status(404).json({ message: "User not found" });
+
+//       // attach user to req
+//       req.user = user;
+
+//       next();
+//     } catch (error) {
+//       console.error("Error in protectRoute middleware", error);
+//       res.status(500).json({ message: "Internal Server Error" });
+//     }
+//   },
+// ];
+
+
+
+
 import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
 
@@ -5,23 +36,30 @@ export const protectRoute = [
   requireAuth(),
   async (req, res, next) => {
     try {
-      const clerkId = req.auth.userId;
+      const clerkId = req.auth().userId;
 
+      if (!clerkId) {
+        return res.status(401).json({
+          message: "Unauthorized - invalid token",
+        });
+      }
 
-      if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
-
-      // find user in db by clerk ID
       const user = await User.findOne({ clerkId });
 
-      if (!user) return res.status(404).json({ message: "User not found" });
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
 
-      // attach user to req
       req.user = user;
 
       next();
     } catch (error) {
-      console.error("Error in protectRoute middleware", error);
-      res.status(500).json({ message: "Internal Server Error" });
+      console.error("Error in protectRoute middleware:", error);
+      return res.status(500).json({
+        message: "Internal Server Error",
+      });
     }
   },
 ];
